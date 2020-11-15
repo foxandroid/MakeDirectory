d16b07051d2794abf1a17bc756c7b30a4846e87f
robin216 <37136276+robin216@users.noreply.github.com>
2020-11-15 13:34:00 +0530

    first commit

--- app/src/main/java/com/example/makedirectory/MainActivity.java
+++ app/src/main/java/com/example/makedirectory/MainActivity.java
@@ -9,10 +9,13 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     EditText folderName;
     Button createFolder;
     String FolderName;
-    private static final int PERMISSION_REQUEST_CODE = 7;
+    private  static final int PERMISSION_REQUEST_CODE = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,69 +33,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         folderName = findViewById(R.id.folderName);
         createFolder = findViewById(R.id.createButton);
-
-        FolderName = folderName.getText().toString();
         createFolder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                FolderName = folderName.getText().toString().trim();
+                if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
 
                     createDirectory(FolderName);
-                }else {
+
+                }else
+                {
 
                     askPermission();
-
                 }
-
 
             }
         });
-
-
 
     }
 
     private void askPermission() {
 
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
-
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
 
-        if (requestCode == PERMISSION_REQUEST_CODE){
-
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == PERMISSION_REQUEST_CODE)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                 createDirectory(FolderName);
-
-            }else {
-
-                Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_SHORT).show();
-
-            }
+            }else
+                {
+                    Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_SHORT).show();
+                }
 
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private void createDirectory(String name){
+    private void createDirectory(String folderName) {
 
-        File file = new File(Environment.getExternalStorageDirectory()+"/" + name);
+        File file = new File(Environment.getExternalStorageDirectory(),folderName);
+
         if (!file.exists()){
 
             file.mkdir();
+
             Toast.makeText(MainActivity.this,"Successful",Toast.LENGTH_SHORT).show();
+        }else
+        {
 
-        }else {
-
-            Toast.makeText(MainActivity.this,"Folder Already exists",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this,"Folder Already Exists",Toast.LENGTH_SHORT).show();
 
 
         }
+
 
     }
 }
